9469edcba3470e728b702ee11d1c576cbfa93d96
wshid <kso4013@gmail.com>
2018-05-24 13:37:51 +0900

    #2-3 Lists with .map

--- src/App.js
+++ src/App.js
@@ -3,11 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 import Movie from './Movie';
 
+const movies = [
+  {
+    title : "Avengers",
+    poster : "https://upload.wikimedia.org/wikipedia/en/4/4d/Avengers_Infinity_War_poster.jpg"
+  },
+  {
+    title : "Guadians",
+    poster : "https://upload.wikimedia.org/wikipedia/en/thumb/a/ab/Guardians_of_the_Galaxy_Vol_2_poster.jpg/220px-Guardians_of_the_Galaxy_Vol_2_poster.jpg"
+  },
+  {
+    title : "StarWars",
+    poster : "https://lumiere-a.akamaihd.net/v1/images/sb_dolby_worldwide_handout_13x19_v3_lg_use_this_one_cc3cc869.jpeg?region=0%2C0%2C821%2C1200"
+  },
+  {
+    title : "DeadPool",
+    poster : "http://www.joblo.com/posters/images/full/deadpool-international.jpg"
+  }
+]
+
+
 class App extends Component {
   render() {
     return ( // 이를 JSX라고 함
       <div className="App"> 
-        <Movie />
+        {movies.map(movie => {
+          return <Movie title = {movie.title} poster = {movie.poster} />
+        })}
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -5,8 +5,8 @@ class Movie extends Component{
     render(){
         return (
             <div>
-                <MoviePoster />
-                <h1>hello this is a movie</h1>
+                <MoviePoster poster = {this.props.poster}/>
+                <h1>{this.props.title}</h1>
             </div>
         );
         
@@ -17,7 +17,7 @@ class Movie extends Component{
 class MoviePoster extends Component{
     render(){
         return(
-            <img src="https://upload.wikimedia.org/wikipedia/en/4/4d/Avengers_Infinity_War_poster.jpg" />
+            <img src={this.props.poster} />
         )
     }
 }
