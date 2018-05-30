ca602a5728f9611a71b40b29b589baee7b171a50
wshid <kso4013@gmail.com>
2018-05-30 19:25:15 +0900

    #6-2. Promises

--- src/App.js
+++ src/App.js
@@ -13,28 +13,11 @@ class App extends Component {
   }
 
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({
-        movies : [
-          {
-            title : "Avengers",
-            poster : "https://upload.wikimedia.org/wikipedia/en/4/4d/Avengers_Infinity_War_poster.jpg"
-          },
-          {
-            title : "Guadians",
-            poster : "https://upload.wikimedia.org/wikipedia/en/thumb/a/ab/Guardians_of_the_Galaxy_Vol_2_poster.jpg/220px-Guardians_of_the_Galaxy_Vol_2_poster.jpg"
-          },
-          {
-            title : "StarWars",
-            poster : "https://lumiere-a.akamaihd.net/v1/images/sb_dolby_worldwide_handout_13x19_v3_lg_use_this_one_cc3cc869.jpeg?region=0%2C0%2C821%2C1200"
-          },
-          {
-            title : "DeadPool",
-            poster : "http://www.joblo.com/posters/images/full/deadpool-international.jpg"
-          }
-        ]
-      })
-    }, 1000)
+    fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating")
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.log(err))
+    
   }
 
   _renderMovies = () => {
